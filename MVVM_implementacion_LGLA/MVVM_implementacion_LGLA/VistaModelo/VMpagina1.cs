8d5cfcb71b7565543d11b097e017184fdb8d0b8f
LuciaLeyva19 <luciagabrielaleyvaalvarez@gmail.com>
2024-11-05 13:26:05 -0700

    Parte 2.3

--- MVVM_implementacion_LGLA/MVVM_implementacion_LGLA/VistaModelo/VMpagina1.cs
+++ MVVM_implementacion_LGLA/MVVM_implementacion_LGLA/VistaModelo/VMpagina1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -10,7 +11,10 @@ namespace MVVM_implementacion_LGLA.VistaModelo
     public class VMpagina1 : BaseViewModel
     {
         #region VARIABLES 
-        string _Texto;
+        string _N1;
+        string _N2;
+        string _R;
+
         #endregion
         #region CONSTRUCTOR
 
@@ -21,26 +25,47 @@ namespace MVVM_implementacion_LGLA.VistaModelo
 
         #endregion
         #region OBJETOS
-        public string Texto
+        public string N1
         {
-            get { return _Texto; }
-            set { SetValue(ref _Texto, value); }
+            get { return _N1; }
+            set { SetValue(ref _N1, value);}
+        }
+        public string N2
+        {
+            get { return _N2; }
+            set { SetValue(ref _N2, value); }
+        }
+        public string R
+        {
+            get { return _R; }
+            set { SetValue(ref _R, value); }
         }
         #endregion
         #region PROCESOS
-        public async Task Alerta()
+        public async Task Procesoasync()
         {
-            await DisplayAlert("Titulo", "Mensaje", "Ok");
-        }
-        public void ProcesoSimple()
-        {
+          
 
+        
+        }
+        public void Sumar()
+        {
+            double n1 = 0;
+            double n2 = 0;
+            double r = 0;
+
+            n1 = Convert.ToDouble(N1);
+            n2 = Convert.ToDouble(N2);
+            r = Convert.ToDouble(R);
+
+            r = n1 + n2;
+            R = r.ToString();
         }
         #endregion
 
         #region COMANDOS
-        public ICommand Alertacommand => new Command(async () => await Alerta());
-        public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
+        public ICommand Alertacommand => new Command(async () => await Procesoasync());
+        public ICommand Suymarcommand => new Command(Sumar);
         #endregion
     }
 }
